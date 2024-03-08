54c9067e669e1047a6f57a6d0c6eb93ddba08572
Sebastian <135719551+sebaswk@users.noreply.github.com>
2024-03-08 09:27:13 -0500

    Update install.sh

--- kubernetes/Loki,FluentBitandGrafana/install.sh
+++ kubernetes/Loki,FluentBitandGrafana/install.sh
@@ -9,6 +9,21 @@ kubectl create namespace $namespace
 #Deploy StorageClass
 kubectl create -f storageclass.yml
 
+#Validate helm installation
+if ! command -v helm &> /dev/null; then
+    echo "Helm is not installed. Please install Helm first"
+    exit 1
+fi
+
+#Validate installation of the grafana repository
+if ! helm repo list | grep -q "grafana"; then
+    echo "Adding the Grafana repository..."
+    helm repo add grafana https://grafana.github.io/helm-charts
+fi
+
+helm repo update
+
+#Grafana deployment
 echo ""
 echo "Deploy Loki, Grafana and Fluent Bit..."
 helm install loki grafana/loki-stack --set grafana.enabled=true,promtail.enabled=false,prometheus.enabled=false,loki.persistence.enabled=false,loki.persistence.storageClassName="standard",loki.service.enabled=true,fluent-bit.enabled=true -n $namespace
